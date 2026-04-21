5efb6765832db47e4555268973ac1a8527826d1c
ztlcoder <ztlcoder@163.com>
2026-04-21 09:41:17 +0800

    删除onetcli

--- scripts/onetcli_uninstaller.ps1
+++
@@ -1,9 +0,0 @@
-$appDataHub = "$env:APPDATA\one-hub"
-
-if (Test-Path $appDataHub) {
-    $item = Get-Item $appDataHub
-    # 仅删除软连接，不伤及 persist 里的原件
-    if ($item.LinkType -eq "Junction") {
-        Remove-Item $appDataHub -Force
-    }
-}
