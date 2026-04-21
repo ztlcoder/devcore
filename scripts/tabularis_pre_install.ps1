64741d23a922e84049618d040f6fa9dff6cc0023
ztlcoder <ztlcoder@163.com>
2026-04-21 09:45:14 +0800

    将tabularis的pre_installer和uninstaller合并到json中

--- scripts/tabularis_pre_install.ps1
+++
@@ -1,8 +0,0 @@
-. "$bucketsdir\devcore\scripts\utils.ps1"
-
-# 1. 重定向 Roaming\debba\tabularis 到 persist\tabularis\debba_data
-Redirect-AppData -RelativePath "debba\tabularis" -PersistDir "$persist_dir\tabularis\debba_data"
-
-# 2. 重定向 Roaming\tabularis 到 persist\tabularis\app_data
-# 同时清理 Local 下的 webview2 缓存
-Redirect-AppData -RelativePath "tabularis" -PersistDir "$persist_dir\tabularis\app_data" -WebViewName "tabularis"
